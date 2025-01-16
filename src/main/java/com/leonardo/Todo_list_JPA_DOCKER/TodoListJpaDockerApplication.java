b59b73c7d464b769d8470af6a1288053392561ab
Leonardo Vinicius <leonardoviniciuswd@gmail.com>
2025-01-16 13:38:28 +0100

    spring initializer

---
+++ src/main/java/com/leonardo/Todo_list_JPA_DOCKER/TodoListJpaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.Todo_list_JPA_DOCKER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListJpaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListJpaDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/Todo_list_JPA_DOCKER/TodoListJpaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.Todo_list_JPA_DOCKER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJpaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
