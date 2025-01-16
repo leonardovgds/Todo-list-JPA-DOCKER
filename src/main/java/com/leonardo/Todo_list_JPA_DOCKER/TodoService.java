0f199cebec96d47f409d953d5f787eeb114eda26
Leonardo Vinicius <leonardoviniciuswd@gmail.com>
2025-01-16 21:09:15 +0100

    created TodoService

---
+++ src/main/java/com/leonardo/Todo_list_JPA_DOCKER/TodoService.java
@@ -0,0 +1,42 @@
+package com.leonardo.Todo_list_JPA_DOCKER;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.leonardo.Todo_list_JPA_DOCKER.entity.Todo;
+import com.leonardo.Todo_list_JPA_DOCKER.repository.TodoRepository;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("priority").descending().and(
+            Sort.by("name").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
